6f4c0dbbcddc0cb9c6227071a2f3f49cd33ce5b0
denystulupov <tulupovdenys@gmail.com>
2021-01-10 10:39:17 +0200

    Adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -6,15 +6,33 @@ $(function(){
     infinite: false,
   });
 
+
+  // аккардион
   $('.questions__item-title').on('click', function(){
     $('.questions__item').removeClass('questions__item--active');
     $(this).parent().addClass('questions__item--active');
   });
 
+
   $('#fullpage').fullpage({
 		autoScrolling:true,
     scrollHorizontally: true,
     sectionSelector: '.page-section',
-	});
+    scrollOverflow: true,
+    menu: '#header__nav',
+    anchors: ["top", "products", "benefits", "specification", "questions", "contacts"],
+  });
   
+  
+  //адаптивное меню
+  $('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list-link').on('click', function(){
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
+  });
+
 });
